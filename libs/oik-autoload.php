bd1c56bd4e3c54c5d6cd8383fcf4a309ca1edd80
Herb Miller <herb@bobbingwide.com>
2015-12-08 16:22:29 +0000

    Add some comments

--- libs/oik-autoload.php
+++ libs/oik-autoload.php
@@ -1,41 +1,44 @@
 <?php // (C) Copyright Bobbing Wide 2015
 if ( !defined( "OIK_AUTOLOAD_LOADED" ) ) {
-define( "OIK_AUTOLOAD_LOADED", "0.0.0" );
-
-
+define( "OIK_AUTOLOAD_LOADED", "0.0.1" );
 
 /**
  * Autoload library functions
  *
  * Library: oik-autoload
  * Provides: oik-autload
- * Type: 
+ * Type: Shared 
  *
  * Implements logic to enable PHP classes to be autoloaded
  * taking into account the libraries that are being used.
  * 
  */
  
+/**
+ * Force "autoload" of a class
+ *
+ * @param string $class - the class to be loaded
+ * @param array $args
+ */
 function oik_require_class( $class, $args=null ) {
 	bw_trace2();
 	$oik_autoload = oik_autoload();
-	bw_trace2( $oik_autoload );
+	bw_trace2( $oik_autoload, "oik_autoload", false, BW_TRACE_VERBOSE );
 	$oik_autoload->autoload( $class );
-	bw_trace2( "done?" );
-
-
+	//bw_trace2( "done?" );
 }
 
 /**
- *
- 
- * The fact that you invoke oik_require_lib( "oik_autoload" ); 
- * should be enough to tell the autoload library that you'll be using autoloading for your classes
- * but I think it's better to implicitely invoke oik_autoload() to instantiate the logic
+ * Load the OIK_Autoload logic
+ * 
+ * You might think that the fact that you invoke oik_require_lib( "oik_autoload" ); 
+ * whould be enough to tell the autoload library that you'll be using autoloading for your classes.
+ * But I think it's better to implicitely invoke either oik_require_class() or oik_autoload() to instantiate the
+ * autoloading logic when you know that OO code will be used.
+ * 
+ * Notice we use oik_require_file() to load a class file manually
  */
-
 function oik_autoload() {
-
 	if ( !class_exists( "OIK_Autoload" ) ) {
 		//echo "Loading OIK_Autoload" ;
 		oik_require_file( "class-oik-autoload.php", "oik-autoload" );
@@ -49,13 +52,6 @@ function oik_autoload() {
 	return( $oik_autoload );
 
 }
-
-
- 
- 
- 
- 
- 
 
 } /* end !defined */
  
