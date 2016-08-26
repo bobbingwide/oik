a5f7ab32041beedaa8d7b15e0e073aab77c08d98
Herb Miller <herb@bobbingwide.com>
2016-08-26 14:50:27 +0100

    Fixes #46 - tests [wp] shortcode displays memory_limit

---
+++ tests/test-oik-bob-bing-wide.php
@@ -0,0 +1,45 @@
+<?php // (C) Copyright Bobbing Wide 2016
+
+/**
+ * @package oik-bob-bing-wide
+ * 
+ * Test the functions in shortcodes/oik-bob-bing-wide.php
+ */
+class Tests_oik_bob_bing_wide extends WP_UnitTestCase {
+
+	function setUp() {
+		parent::setUp();
+		oik_require( "shortcodes/oik-bob-bing-wide.php" );
+	}
+
+	/**
+	 * Unit test equivalent to [wp v p m]
+	 */
+	function test_bw_wp_v0_p1_m2() {
+		$expected_output = '<span class="wordpress"><span class="bw_word">Word</span><span class="bw_press">Press</span>';
+		global $wp_version;
+		$expected_output .= " $wp_version";
+		$expected_output .= ". PHP: ";
+    $expected_output .= phpversion();
+    $memory_limit = ini_get( "memory_limit" );
+		$expected_output .= ". Memory limit: " . $memory_limit;
+		$expected_output .= '</span>';
+	
+		$html = bw_wp( array( "v", "p", "m" ) );
+		$this->assertEquals( $expected_output, $html );
+	}
+	
+	/**
+	 * Unit test equivalent to [wp v m=y]
+	 * 
+	 * Cheating here. We're not testing that the WordPress version is included by PHP version is not.
+	 */
+	function test_bw_wp_v0_my() {
+    $memory_limit = ini_set( "memory_limit", "256M" );
+		$expected_output = null;
+		$expected_output .= ". Memory limit: 256M";
+		$expected_output .= '</span>';
+		$html = bw_wp( array( "v", "m" => "y" ) );
+		$this->assertStringEndsWith( $expected_output, $html ); 
+	}
+}
