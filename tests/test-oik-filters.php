c01d4ec8db2542fd8a0f844a16dd94f516123c60
Herb Miller <herb@bobbingwide.com>
2016-12-06 13:05:53 +0000

    Issue #58 - test bw_replace_filter and bw_disable_filter

---
+++ tests/test-oik-filters.php
@@ -0,0 +1,48 @@
+<?php // (C) Copyright Bobbing Wide 2016
+
+/**
+ * @package oik-bob-bing-wide
+ * 
+ * Test the functions in shortcodes/oik-bob-bing-wide.php
+ */
+class Tests_oik_filters extends BW_UnitTestCase {
+
+	function setUp() {
+		parent::setUp();
+		oik_require( "includes/oik-filters.inc" );
+	}
+	
+	static function it_worked( $parm1, $parm2, $parm3) {
+		$parm1 = "It worked!";
+		return( $parm1 );
+	}
+
+	/**
+	 * Unit test to demonstrate bw_replace_filter
+	 * If it works then we get "It worked!" else we gobang()
+	 * 
+	 * Note: This test works when the function being replaced is not defined as a class method!
+	 */
+	function test_bw_replace_filter() {
+		add_filter( "test_replace", array( $this, "gob" ), 8, 3 );
+		bw_replace_filter( "test_replace", array( $this, "gob" ), 8, array( $this, "it_worked") );
+		$html = apply_filters( "test_replace", "How's it going?", "2", "3" );
+		$expected_output = "It worked!";
+		$this->assertEquals( $expected_output, $html );
+	}
+	
+	/**
+	 * Unit test to demonstrate bw_disable_filter
+	 */
+	function test_bw_disable_filter() {
+		add_filter( "test_replace", "Tests_oik_filters::it_worked", 8, 3 );
+		$result1 = apply_filters( "test_replace", "How's it going?", "2", "3" );
+		$this->assertEquals( "It worked!", $result1 );
+		bw_disable_filter( "test_replace", "Tests_oik_filters::it_worked", 8 );
+		$result2 = apply_filters( "test_replace", "How's it going?", "2", "3" );
+		$this->assertEquals( "How's it going?", $result2 );
+	}
+		
+		
+
+}
