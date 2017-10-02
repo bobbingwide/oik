14ca0e71185fb349851e84e8b25243beb7d80181
Herb Miller <herb@bobbingwide.com>
2017-10-02 14:07:10 +0100

    Issue #9 - convert bw_example and test en_GB and bb_BB

---
+++ tests/test-shortcodes-oik-bw.php
@@ -0,0 +1,54 @@
+<?php // (C) Copyright Bobbing Wide 2017
+
+/**
+ * @package 
+ * 
+ * Tests for logic in shortcodes/oik-bw.php
+ */
+class Tests_shortcodes_oik_bw extends BW_UnitTestCase {
+
+	function setUp() { 
+		parent::setUp();
+		
+		//oik_require_lib( "oik-sc-help" );
+		oik_require( "shortcodes/oik-bw.php" ); 														
+	}
+	
+	function test_bw__syntax() {
+		$this->switch_to_locale( "en_GB" );
+		$array = bw__syntax();
+		$html = $this->arraytohtml( $array, true );
+		//$this->generate_expected_file( $html );
+		$this->assertArrayEqualsFile( $html );
+	}
+	
+	function test_bw__syntax_bb_BB() {
+		$this->switch_to_locale( "bb_BB" );
+		$array = bw__syntax();
+		$html = $this->arraytohtml( $array, true );
+		//$this->generate_expected_file( $html );
+		$this->assertArrayEqualsFile( $html );
+		$this->switch_to_locale( "en_GB" );
+	}
+
+	function test_bw__example() {
+		$this->setExpectedDeprecated( "bw_translate" );
+		$this->switch_to_locale( "en_GB" );
+		$html = bw_ret( bw__example() );
+		//$this->generate_expected_file( $html );
+		$this->assertArrayEqualsFile( $html );
+	}
+	
+	function test_bw__example_bb_BB() {
+		$this->setExpectedDeprecated( "bw_translate" );
+		$this->switch_to_locale( "bb_BB" );
+		$html = bw_ret( bw__example() );
+		//$this->generate_expected_file( $html );
+		$this->assertArrayEqualsFile( $html );
+		$this->switch_to_locale( "en_GB" );
+	}
+	
+	
+	
+}
+	
