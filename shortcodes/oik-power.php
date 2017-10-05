eb8f019bf716c5fe53e696e501860b6a7dc81b71
Herb Miller <herb@bobbingwide.com>
2017-10-05 18:29:51 +0100

    Convert bw_power and test en_GB and bb_BB

--- shortcodes/oik-power.php
+++ shortcodes/oik-power.php
@@ -1,12 +1,12 @@
-<?php // (C) Copyright Bobbing Wide 2012-2014
+<?php // (C) Copyright Bobbing Wide 2012-2017
 
 /**
- * Implement [bw_power] shortcode
+ * Implements [bw_power] shortcode
  * 
  * Display the "Proudly powered by WordPress" link to WordPress.org
  * 
  * `
- * <a href="http://wordpress.org/" title="Semantic Personal Publishing Platform" rel="generator">Proudly powered by WordPress</a>
+ * <a href="https://wordpress.org/" title="Semantic Personal Publishing Platform" rel="generator">Proudly powered by WordPress</a>
  * `
  *
  * @param array $atts - shortcode parameters - none expected
@@ -15,6 +15,6 @@
  * @return string - the generated HTML
  */
 function bw_power( $atts=null, $content=null, $tag=null ) {
-  alink( "bw_power", "http://www.wordpress.org", __("Proudly powered by WordPress"), __("Semantic Personal Publishing Platform" ) );
+  BW_::alink( "bw_power", "https://www.wordpress.org", __( "Proudly powered by WordPress", "oik" ), __( "Semantic Personal Publishing Platform", "oik" ) );
   return( bw_ret());
 }
---
+++ tests/test-shortcodes-oik-power.php
@@ -0,0 +1,35 @@
+<?php // (C) Copyright Bobbing Wide 2016,2017
+
+/**
+ * @package 
+ * 
+ * Tests for logic in shortcodes/oik-power.php
+ */
+class Tests_shortcodes_oik_power extends BW_UnitTestCase {
+
+	function setUp() { 
+		parent::setUp();
+		
+		oik_require_lib( "oik-sc-help" );
+		oik_require( "shortcodes/oik-power.php" ); 														
+	}
+	
+	function test_bw_power() {
+		$this->switch_to_locale( "en_GB" );
+		$html = bw_power();
+		//$this->generate_expected_file( $html );
+		$this->assertArrayEqualsFile( $html );
+		$this->switch_to_locale( "en_GB" );
+	}
+	
+	function test_bw_power_bb_BB() {
+		$this->switch_to_locale( "bb_BB" );
+		$html = bw_power();
+		//$this->generate_expected_file( $html );
+		$this->assertArrayEqualsFile( $html );
+		$this->switch_to_locale( "en_GB" );
+	}
+
+	
+	
+}
