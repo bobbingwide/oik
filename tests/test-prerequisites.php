16b25ff7257c3f633793134fa10d1494ccda9790
Herb Miller <herb@bobbingwide.com>
2017-10-16 09:51:03 +0100

    Issue #9 - start adding logic to test for pre-requisite environmental conditions

---
+++ tests/test-prerequisites.php
@@ -0,0 +1,100 @@
+
+<?php // (C) Copyright Bobbing Wide 2017
+
+
+class Tests_prerequisites extends BW_UnitTestCase {
+
+	/** 
+	 * set up logic
+	 * 
+	 * - ensure any database updates are NOT rolled back
+	 * - if you don't call parent::setUp then $this->setExpectedDeprecated() won't work; as the deprecation checking is not enabled
+	 */
+	function setUp() {
+		// parent::setUp();
+		oik_require( "includes/bw_posts.inc" );
+		
+	}
+	
+	/**
+	 * The dummy posts that we create will be pages with "post title" and optional suffix n
+	 * We don't want these to be in the database otherwise we'll get invalid suffices in the permalinks
+	 * 
+	 * They can't be in trash either!
+	 */
+	function test_delete_pages_with_post_title() {
+	
+		$this->delete_posts_with_post_title( "post title" );
+		$this->delete_posts_with_post_title( "post title", "trash" );
+		$this->delete_posts_with_post_title( "post title 1" );
+		$this->delete_posts_with_post_title( "post title 1", "trash" );
+		$this->delete_posts_with_post_title( "post title 2" );
+		$this->delete_posts_with_post_title( "post title 2", "trash" );
+		$this->delete_posts_with_post_title( "post title 3" );
+		$this->delete_posts_with_post_title( "post title 3", "trash" );
+		
+		$this->test_no_posts();
+	}
+	
+	function test_no_posts() {
+		$this->check_no_posts( "post title" );
+		$this->check_no_posts( "post title", "trash" );
+		$this->check_no_posts( "post title 1" );
+		$this->check_no_posts( "post title 1", "trash" );
+		$this->check_no_posts( "post title 2" );
+		$this->check_no_posts( "post title 2", "trash" );		
+		$this->check_no_posts( "post title 3" );
+		$this->check_no_posts( "post title 3", "trash" );
+	}
+	
+	
+	function get_posts( $post_title, $post_status="any" ) {
+		$atts = array( "post_type" => "page" 
+								 , "numberposts" => -1
+								 , "title" => $post_title
+								 , "post_parent" => "."
+								 , "post_status" => $post_status
+								 );
+		$posts = bw_get_posts( $atts );
+		return $posts;
+	}
+	
+	/**
+	 * Checks there are no matching posts
+	 */
+	function check_no_posts( $post_title, $post_status="any" ) {
+		$posts = $this->get_posts( $post_title, $post_status );
+		$this->assertEquals( array(), $posts );
+	}
+	
+	/**
+	 *  post_status = "any" does not include trash auto-draft or spam
+	 *  wp_posts.post_status <> 'trash' AND wp_posts.post_status <> 'auto-draft' AND wp_posts.post_status <> 'spam')
+	 *
+	 * If this method finds a post it will issue a message, which makes this test Risky.
+	 * But we commit, so the next run and subsequent runs should be OK.
+	 */
+	function delete_posts_with_post_title( $post_title, $post_status="any" ) {
+		$posts = $this->get_posts( $post_title, $post_status );
+		foreach ( $posts as $post ) {
+			if ( $post->post_title == $post_title ) {
+				echo "Deleting {$post->ID} {$post->post_title} " . PHP_EOL;
+				wp_delete_post( $post->ID, true );
+			}
+		}
+		$this->commit_transaction();
+	}
+	
+	/** 
+	 * Forcibly commit the transaction
+	 */
+	public static function commit_transaction() {
+		global $wpdb;
+		$wpdb->query( 'COMMIT' );
+		bw_trace2( $wpdb, "wpdb" );
+	}
+	
+	
+	
+}
+	
