54b2ad63fdf4fb13b755f3f23e0522f4ad22225c
Herb Miller <herb@bobbingwide.com>
2017-11-29 11:20:43 +0000

    Issue #9 - add tests for bw_address, en_GB and bb_BB - demonstrating issue #89

---
+++ tests/test-shortcodes-oik-address.php
@@ -0,0 +1,100 @@
+<?php // (C) Copyright Bobbing Wide 2016,2017
+
+/**
+ * @package 
+ * 
+ * Tests for logic in shortcodes/oik-address.php
+ */
+class Tests_shortcodes_oik_address extends BW_UnitTestCase {
+
+	function setUp() { 
+		parent::setUp();
+		oik_require( "shortcodes/oik-address.php" );
+		oik_require_lib( "oik-sc-help" ); 														
+	}
+	
+	function test_bw_address__syntax() {
+		$this->switch_to_locale( "en_GB" );
+		$array = bw_address__syntax();
+		$html = $this->arraytohtml( $array, true );
+		//$this->generate_expected_file( $html );
+		$this->assertArrayEqualsFile( $html );
+	}
+	
+	function test_bw_address__syntax_bb_BB() {
+		$this->switch_to_locale( "bb_BB" );
+		$array = bw_address__syntax();
+		$html = $this->arraytohtml( $array, true );
+		//$this->generate_expected_file( $html );
+		$this->assertArrayEqualsFile( $html );
+		$this->switch_to_locale( "en_GB" );
+	}
+	
+	/**
+	 * This tests the output from the shortcode. 
+	 * The 'the_content' filter is not applied so wpautop() does not mess with it.
+	 */
+	function test_bw_address() {
+		$this->switch_to_locale( "en_GB" );
+		$atts = array(); 
+		$html = bw_address( $atts);
+		$html_array = $this->tag_break( $html ); 
+		$this->assertNotNull( $html_array );
+		//$this->generate_expected_file( $html_array );
+		$this->assertArrayEqualsFile( $html_array );
+	}
+	
+	function test_bw_address_bb_BB() {
+		$this->switch_to_locale( "bb_BB" );
+		$atts = array(); 
+		$html = bw_address( $atts);
+		$html_array = $this->tag_break( $html ); 
+		$this->assertNotNull( $html_array );
+		//$this->generate_expected_file( $html_array );
+		$this->assertArrayEqualsFile( $html_array );
+		$this->switch_to_locale( "en_GB" );
+	}
+	
+	/**
+	 * In this test 'the_content' filter messes with our nicely crafted HTML,
+	 * adding an unwanted p tag but no end tag.
+	 */
+	function test_bw_address__example() {
+		$this->switch_to_locale( "en_GB" );
+		$html = bw_ret( bw_address__example() );
+		//$this->generate_expected_file( $html );
+		$this->assertArrayEqualsFile( $html );
+	}
+	
+	function test_bw_address__example_bb_BB() {
+		$this->switch_to_locale( "bb_BB" );
+		$html = bw_ret( bw_address__example() );
+		//$this->generate_expected_file( $html );
+		$this->assertArrayEqualsFile( $html );
+		$this->switch_to_locale( "en_GB" );
+	}
+	
+	/**
+	 * How do we test the snippet logic for bw_address? 
+	 */
+	function test_bw_address__snippet() {
+		$this->switch_to_locale( "en_GB" );
+		bw_lazy_sc_snippet( "bw_address" );
+		$html = bw_ret();
+		//$this->generate_expected_file( $html );
+		$this->assertArrayEqualsFile( $html );
+	}
+	
+	function test_bw_address__snippet_bb_BB() {
+		$this->switch_to_locale( "bb_BB" );
+		bw_lazy_sc_snippet( "bw_address" );
+		$html = bw_ret();
+		//$this->generate_expected_file( $html );
+		$this->assertArrayEqualsFile( $html );
+		$this->switch_to_locale( "en_GB" );
+	}
+	
+
+	
+}
+	
