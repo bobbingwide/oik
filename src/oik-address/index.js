6e00fe418c6adf41874747ef470b4234a93c9e8f
Herb Miller <herb@bobbingwide.com>
2021-08-13 11:35:22 +0100

    Refactor oik/address to use wp-scripts and block.json #177

--- src/oik-address/index.js
+++ src/oik-address/index.js
@@ -4,126 +4,82 @@
  * - Depends on oik
  * -
  *
- * @copyright (C) Copyright Bobbing Wide 2018,2019, 2020
+ * @copyright (C) Copyright Bobbing Wide 2018,2019,2020,2021
  * @author Herb Miller @bobbingwide
  */
 
 import './style.scss';
 import './editor.scss';
 
-//import Input from './input';
-//import TextControl from '@wordpress/components-text
+import { __ } from '@wordpress/i18n';
+import classnames from 'classnames';
 
-// Get just the __() localization function from wp.i18n
-const { __ } = wp.i18n;
-// Get registerBlockType and Editable from wp.blocks
-const { registerBlockType, Editable } = wp.blocks;
-
-const {
-    ServerSideRender,
-} = wp.editor;
-const {
-    InspectorControls,
-} = wp.blockEditor;
-
-const {
+import { registerBlockType, createBlock } from '@wordpress/blocks';
+import {AlignmentControl, BlockControls, InspectorControls, useBlockProps, PlainText, BlockIcon} from '@wordpress/block-editor';
+import ServerSideRender from '@wordpress/server-side-render';
+import {
     Toolbar,
     PanelBody,
     PanelRow,
     FormToggle,
     TextControl,
-    SelectControl,
-} = wp.components;
-const Fragment = wp.element.Fragment;
+    TextareaControl,
+    SelectControl } from '@wordpress/components';
+import { Fragment} from '@wordpress/element';
 import { map, partial } from 'lodash';
-// Set the header for the block since it is reused
-//const blockHeader = <h3>{ __( 'Address' ) }</h3>;
 import { transforms } from './transforms.js';
 
-//var TextControl = wp.blocks.InspectorControls.TextControl;
 /**
  * These are the different options for the tag attr
  */
 const tagOptions =
     { "div": "Block",
-        "span": "Inline",
-
+       "span": "Inline",
     };
 
 /**
- * Register e
+ * Register the block
  */
 export default registerBlockType(
     // Namespaced, hyphens, lowercase, unique name
     'oik/address',
     {
-        // Localize title using wp.i18n.__()
-        title: __( 'Address' ),
-				
-		description: 'Displays your address from oik-options',
-
-        // Category Options: common, formatting, layout, widgets, embed
-        category: 'common',
-
-        // Dashicons Options - https://goo.gl/aTM1DQ
-        icon: 'building',
-
-        // Limit to 3 Keywords / Phrases
-        keywords: [
-            __( 'Address' ),
-            __( 'oik' ),
-        ],
-
-        // Set for each piece of dynamic data used in your block
-        attributes: {
-            tag: {
-                type: 'string',
-                default: 'div',
-            }
-
-					
-        },
         example: {
         },
         transforms,
 
-        supports: {
-            customClassName: false,
-            className: false,
-            html: false,
-        },
-
-        edit: props => {
+       edit: props => {
+           const { attributes, setAttributes, instanceId, focus, isSelected } = props;
+           const { textAlign, label } = props.attributes;
+           const blockProps = useBlockProps( {
+               className: classnames( {
+                   [ `has-text-align-${ textAlign }` ]: textAlign,
+               } ),
+           } );
             function onChangeAttr( key, value ) {
                 props.setAttributes( { [key] : value } );
             };
 
-
-					
           return (
-
-              <Fragment>
-              <InspectorControls >
-              <PanelBody>
-            <PanelRow>
-                <SelectControl label="Display" value={props.attributes.tag}
+            <Fragment>
+                <InspectorControls >
+                    <PanelBody>
+                        <PanelRow>
+                            <SelectControl label="Display" value={props.attributes.tag}
                                      options={ map( tagOptions, ( key, label ) => ( { value: label, label: key } ) ) }
                                      onChange={partial( onChangeAttr, 'tag' )}
-            />
-
-            </PanelRow>
-                  <PanelRow>
-                      Equivalent shortcode<br />
-                      &#91;bw_address tag={props.attributes.tag}&#93;
-                  </PanelRow>
-            </PanelBody>
-
-        </InspectorControls>
-
-        <ServerSideRender
-            block="oik/address" attributes={ props.attributes }
-            />
-        </Fragment>
+                            />
+                        </PanelRow>
+                        <PanelRow>
+                            Equivalent shortcode<br />
+                            &#91;bw_address tag={props.attributes.tag}&#93;
+                        </PanelRow>
+                    </PanelBody>
+                </InspectorControls>
+                <div { ...blockProps}>
+                    <ServerSideRender block="oik/address" attributes={ props.attributes } />
+                </div>
+            </Fragment>
           );
         },
 
