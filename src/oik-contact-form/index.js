26e02480e3f78d82bf5474e0853f55095c6c962a
Herb Miller <herb@bobbingwide.com>
2021-08-13 12:19:21 +0100

    Refactor oik/contact-form for wp-scripts and block.json #177

--- src/oik-contact-form/index.js
+++ src/oik-contact-form/index.js
@@ -3,94 +3,59 @@
  *
  * Equivalent to [bw_contact_form]
  *
- * @copyright (C) Copyright Bobbing Wide 2018, 2020
+ * @copyright (C) Copyright Bobbing Wide 2018, 2020, 2021
  * @author Herb Miller @bobbingwide
  */
 import './style.scss';
 import './editor.scss';
 
-//import Input from './input';
-//import TextControl from '@wordpress/components-text
 
-// Get just the __() localization function from wp.i18n
-const { __ } = wp.i18n;
-// Get registerBlockType and Editable from wp.blocks
-const { 
-	registerBlockType, 
-	Editable,
- } = wp.blocks;
-const {
-    InspectorControls,
-} = wp.blockEditor;
-	 
-const {
-  Toolbar,
-  Button,
-  Tooltip,
-  PanelBody,
-  PanelRow,
-  FormToggle,
-	TextControl,
+import { __ } from '@wordpress/i18n';
+import classnames from 'classnames';
 
-} = wp.components;
-const {
-    ServerSideRender,
-} = wp.editor;
+import { registerBlockType, createBlock } from '@wordpress/blocks';
+import {AlignmentControl, BlockControls, InspectorControls, useBlockProps, PlainText, BlockIcon} from '@wordpress/block-editor';
+import ServerSideRender from '@wordpress/server-side-render';
+import {
+    Toolbar,
+    PanelBody,
+    PanelRow,
+    FormToggle,
+    TextControl,
+    TextareaControl,
+    SelectControl } from '@wordpress/components';
+import { Fragment} from '@wordpress/element';
+import { map, partial } from 'lodash';
 import { transforms } from './transforms.js';
-
-
-
 
 registerBlockType(
     // Namespaced, hyphens, lowercase, unique name
 		'oik/contact-form',
     {
-        // Localize title using wp.i18n.__()
-        title: __( 'Contact form' ),
-				
-				description: 'Displays a Contact form',
-
-        // Category Options: common, formatting, layout, widgets, embed
-        category: 'common',
-
-        // Dashicons Options - https://goo.gl/aTM1DQ
-        icon: 'forms',
-
-        // Limit to 3 Keywords / Phrases
-        keywords: [
-            __( 'Contact' ),
-			__( 'Form' ),
-            __( 'oik' ),
-        ],
         example: {
         },
         transforms,
 
-        // Set for each piece of dynamic data used in your block
-        attributes: {
-		/*
-          user: {
-            type: 'string',
-          },
-
-		 */
-					
-        },
-			
-				edit: props=> {
-					
+		edit: props=> {
+            const { attributes, setAttributes, instanceId, focus, isSelected } = props;
+            const { textAlign, label } = props.attributes;
+            const blockProps = useBlockProps( {
+                className: classnames( {
+                    [ `has-text-align-${ textAlign }` ]: textAlign,
+                } ),
+            } );
           return (
-					
+					<div { ...blockProps}>
                        <ServerSideRender
-                    block="oik/contact-form" attributes={ props.attributes }
+                        block="oik/contact-form" attributes={ props.attributes }
                     />
+                    </div>
           );
         },
 				
-			save() {
+		save() {
 				 // Rendering in PHP
 					return null;
-			},
+		}
     }
 );
-
