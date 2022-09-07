4332cfeb6272ba0afb90a409eed81c5fe57e2376
Herb Miller <herb@bobbingwide.com>
2022-09-07 10:45:44 +0100

    Add oik-contact-field then note in the comments to use webpack.confg.js #207

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@
  *
  * Each shortcode block is in a separate folder within src
  * From oik v4.8.0 each block has its own entry point.
- * This file has been superseded
+ * This file has been superseded by webpack.config.js
  * 
  * @copyright (C) Copyright Bobbing Wide 2018, 2019, 2020, 2021, 2022
  * @author Herb Miller @bobbingwide
  */
 import './oik-address';		 // From oik & oik-user
 import './oik-contact-form'; // From oik bw_contact_form
+import './oik-contact-field'; // Nested block for oik/contact-form
 import './oik-content';      // Dynamic content block
 import './oik-countdown';    // From oik bw_countdown
 import './oik-follow-me';    // From oik bw_follow_me
