7cc697f97ee4e9913965587894a48f9d20b57ebc
Herb Miller <herb@bobbingwide.com>
2022-09-10 11:18:43 +0100

    Add oik/contact-field block - inner block to oik/contact-form #207

---
+++ build/contact-field.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => '5ffeb721bb3cd15feae1');
--- build/contact-form.asset.php
+++ build/contact-form.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => '415bb0ff9218c6b64ee5');
+<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => 'e2c5003584604a478e5b');
