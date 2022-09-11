d159effe650adcc5c4cac6ef5b440641e60fa713
Herb Miller <herb@bobbingwide.com>
2022-09-11 10:32:14 +0100

    Update language files #207

--- build/contact-field.asset.php
+++ build/contact-field.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => '5ffeb721bb3cd15feae1');
+<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => '5ebde217cfb6dc9d5fac');
