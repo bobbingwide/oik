7c1c1cdc6ed08f297c82b38627f6118efe59407d
Herb Miller <herb@bobbingwide.com>
2022-09-12 13:41:11 +0100

    Update build files #207

--- build/contact-form.asset.php
+++ build/contact-form.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => '08b67108fefa615381ca');
+<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => '4933b59e816992dcde1e');
